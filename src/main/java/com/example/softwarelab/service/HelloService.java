d5565e7201e4c13ea263d866c48eacf870a8de22
Esha Saha <sahaesha66@gmail.com>
2026-02-21 18:56:22 +0600

    Update hello message for PR

--- src/main/java/com/example/softwarelab/service/HelloService.java
+++ src/main/java/com/example/softwarelab/service/HelloService.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
+    // HelloService.java
     public String getMessage() {
-        return "Hello Software Lab!";
+        return "Hello Software Lab! [Updated]"; // add [Updated] or any small text
     }
 }
